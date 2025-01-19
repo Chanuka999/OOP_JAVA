64eba0e8e86c7b4206d21e0f9d188a8bb8559069
Chanuka999 <chanukaranditha99@gmail.com>
2025-01-19 17:28:48 +0530

    update

---
+++ new/Book.java
@@ -0,0 +1,15 @@
+public class Book{
+  private int bookId;
+  private String title;
+  private String author;
+  private boolean available;
+  
+  Book(int bookId,String title,String author,boolean available){
+    this.bookId=bookId;
+	this.title=title;
+	this.author=author;
+	this.available=available;
+  }
+  
+  
+}
